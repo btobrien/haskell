a42c9787f91beb674ad385ae6feb1e0c5fdd6beb
Bret O'Brien <bobrien@umich.edu>
2018-12-02 23:52:35 -0600

    monadic parsing -- Ch13

---
+++ excersises/Ch13.hs
@@ -0,0 +1,138 @@
+
+-- in this chapter we illustrate how monad can be used to implement parsers
+-- What is a parser? 
+--  A parser is a program that takes a string of chars as input, and produces some form of tree that makes the syntactic structure of the string explicit
+
+import Control.Applicative
+import Data.Char
+import Data.Maybe
+
+newtype Parser a = P (String -> Maybe (a,String))
+
+parse :: Parser a -> String -> Maybe (a,String)
+parse (P p) inp = p inp
+
+item :: Parser Char
+item = P (\inp -> case inp of
+    [] -> Nothing 
+    (x:xs) -> Just (x,xs))
+
+-- Sequencing Parsers
+
+instance Functor Parser where
+-- fmap :: (a -> b) -> Parser a -> Parser b
+    fmap g p = P (\inp -> case parse p inp of
+        Nothing -> Nothing
+        Just (v,out) -> Just (g v, out))
+
+instance Applicative Parser where
+--  pure :: a -> Parser a
+    pure v = P (\inp -> Just (v,inp))
+--  <*> :: Parser (a -> b) -> Parser a -> Parser b
+    pg <*> px = P (\inp -> case parse pg inp of
+        Nothing -> Nothing
+        Just (g,out) -> parse (fmap g px) out)
+
+instance Monad Parser where
+--  (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+    p >>= f = P (\inp -> case parse p inp of
+        Nothing -> Nothing
+        Just (x,out) -> parse (f x) out)
+
+instance Alternative Parser where
+--  empty :: Parser a
+    empty = P $ const Nothing
+--  <|> :: Parser a -> Parser a -> Parser a
+    px <|> py = P (\inp -> case parse px inp of
+        Nothing -> parse py inp
+        x -> x)
+
+readParser :: Read a => Parser String -> Parser a
+readParser = fmap read
+
+-- Derived Primitives
+
+sat :: (Char -> Bool) -> Parser Char
+sat p = do
+    x <- item
+    if p x then return x else empty
+
+digit :: Parser Char
+digit = sat isDigit
+
+lower :: Parser Char
+lower =  sat isLower
+
+upper :: Parser Char
+upper =  sat isUpper
+
+letter :: Parser Char
+letter = sat isAlpha
+
+alphanum :: Parser Char
+alphanum = sat isAlphaNum
+
+char :: Char -> Parser Char
+char x = sat (==x)
+
+string :: String -> Parser String
+string = foldr (\x p -> do char x; fmap (x:) p) (return [])
+
+ident :: Parser String
+ident = (:) <$> lower <*> (many alphanum)
+
+space :: Parser ()
+space = many (sat isSpace) >> return ()
+
+nat :: Parser Int
+nat = readParser (some digit)
+
+neg :: Parser Int
+neg = char '-' >> fmap (negate) nat
+
+int :: Parser Int
+int = nat <|> neg
+
+token :: Parser a -> Parser a
+token p = do
+    space
+    v <- p
+    space
+    return v
+
+identifier :: Parser String
+identifier = token ident
+
+natural :: Parser Int
+natural = token nat
+
+negative :: Parser Int
+negative = token neg
+
+integer :: Parser Int
+integer = token int
+
+symbol :: String -> Parser String
+symbol = token . string
+
+emptyList :: Parser [a]
+emptyList = symbol "[" >> symbol "]" >> return []
+
+nonEmptyListOf :: Parser a -> Parser [a]
+nonEmptyListOf p =  do
+    symbol "["
+    x <- p
+    xs <- many (symbol "," >> p)
+    symbol "]"
+    return (x:xs)
+
+listOf :: Parser a -> Parser [a]
+listOf p = emptyList <|> nonEmptyList p
+
+integers :: Parser [Int]
+integers = listOf integer
+
+main = do
+    inp <- getContents
+    print . fmap fst . parse (some $ string "abc\n") $ inp
+
