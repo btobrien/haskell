78974f1727c3e12abfc52adacb45d16804b97d7c
Bret O'Brien <bobrien@wolve.com>
2019-11-01 10:05:57 -0500

    refactor repeated squares

--- abstract_algebra/RepeatedSquares.hs
+++ abstract_algebra/RepeatedSquares.hs
@@ -13,11 +13,8 @@ prodMod n x = (`mod`n) . (x*)
 repeatedSquares :: Int -> Int -> [Int]
 repeatedSquares n = iterate ((`mod`n) . (^2))
 
-powerMod' :: Int -> Int -> Int -> Int
-powerMod' n base exp = foldr (prodMod n) 1 $ zipFilter (binary exp) (repeatedSquares n base)
-
 powerMod :: Int -> Int -> Int -> Int
-powerMod n base exp = powerMod' n (mod base n) (mod exp n)
+powerMod n base exp = foldr (prodMod n) 1 $ zipFilter (binary exp) (repeatedSquares n base)
 
 zipFilter :: [Bool] -> [a] -> [a]
 zipFilter bs = map snd . filter fst . zip bs
