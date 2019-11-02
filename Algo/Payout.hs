e1cf7d0155270cf6bc5bb161f34bb854609362c7
Bret O'Brien <bobrien@wolve.com>
2019-11-01 19:06:18 -0500

    refactored payouts

--- Algo/Payout.hs
+++ Algo/Payout.hs
@@ -1,43 +1,32 @@
 
 import Data.Either
 import Data.List
-import Control.Applicative
 
-type Name = String
-type Result = (Int, Name); resultOf = fst; nameOf = snd; 
-type Payout = (Int, (Name, Name)) --; payoutOf = fst; namesOf = snd
-type Missing = Int
+type Result = (Int, String); valueOf = fst; nameOf = snd; 
+type Payout = (Int, (String, String))
 
-readResult :: String -> Result
-readResult = (\(name:val:_) -> (read val, name)) . words
-
-showPayout :: Payout -> String
-showPayout (val, (from, to)) = from ++ " -> " ++ to ++ ": " ++ (show val)
-
-showMissing :: Missing -> String
-showMissing val = "ERROR: results add to " ++ show val 
-
-main = do 
-    inp <- getContents
-    let results = map readResult . lines $ inp
-    putStrLn . either showMissing (unlines . map showPayout) . balance $ results
-    
-balance :: [Result] -> Either Missing [Payout] 
-balance results = if net /= 0 then (Left net) else Right (unfoldr payout splitResults)
+main = getContents >>= putStrLn . either showError showPayouts . calculatePayouts . readResults
     where
-    net = sum (map resultOf results)
-    splitResults = partition ((<0) . resultOf) results
+    readResults = map ((\(name:val:_) -> (read val, name)) . words) . lines
+    showPayouts = unlines . map (\(val, (from, to)) -> from ++ " -> " ++ to ++ ": " ++ show val)
+    showError = ("ERROR: results add to "++) . show 
+    
+calculatePayouts :: [Result] -> Either Int [Payout] 
+calculatePayouts results = if net /= 0 then (Left net) else Right (unfoldr selectPayout (losers,winners))
+    where
+    net = sum . map valueOf $ results
+    (losers,winners) = partition ((<0) . valueOf) results
 
-payout :: ([Result], [Result]) -> Maybe (Payout, ([Result],[Result]))
-payout (losers,winners) = if null winners || null losers then Nothing else Just (payout,adjusted)
+selectPayout :: ([Result], [Result]) -> Maybe (Payout, ([Result],[Result]))
+selectPayout (losers,winners) = if null winners || null losers then Nothing else Just (payout, adjusted)
     where
     winner = maximum winners
     loser = minimum losers
-    amount = min (abs (resultOf loser)) (resultOf winner) 
+    amount = min (abs . valueOf $ loser) (valueOf winner) 
     payout = (amount, (nameOf loser, nameOf winner))
     adjusted = (,) 
-        (adjust (nameOf loser) (resultOf loser + amount) losers)
-        (adjust (nameOf winner) (resultOf winner - amount) winners)
+        (adjust (nameOf loser)  (valueOf loser  + amount) losers)
+        (adjust (nameOf winner) (valueOf winner - amount) winners)
 
-adjust :: Name -> Int -> [Result] -> [Result]
-adjust name new = filter ((0/=) . resultOf) . ((new,name):) . filter ((name/=) . nameOf)
+adjust :: String -> Int -> [Result] -> [Result]
+adjust name new = filter ((0/=) . valueOf) . ((new,name):) . filter ((name/=) . nameOf)
