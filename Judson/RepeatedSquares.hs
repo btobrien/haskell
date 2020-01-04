f1a1f6b91bbee40e734c823802c4a932830ad13b
Bret O'Brien <bobrien@umich.edu>
2020-01-04 17:58:42 -0600

    renamed toInt to fromBinary

--- Judson/RepeatedSquares.hs
+++ Judson/RepeatedSquares.hs
@@ -4,8 +4,8 @@ import Data.List
 binary :: Int -> [Bool]
 binary = unfoldr (\n -> if n==0 then Nothing else Just (odd n, n `div` 2))
 
-toInt :: [Bool] -> Int
-toInt = foldr (\b -> (fromEnum b +) . (2*)) 0
+fromBinary :: [Bool] -> Int
+fromBinary = foldr (\b -> (fromEnum b +) . (2*)) 0
 
 prodMod :: Int -> Int -> Int -> Int
 prodMod n x = (`mod`n) . (x*)
@@ -14,7 +14,8 @@ repeatedSquares :: Int -> Int -> [Int]
 repeatedSquares n = iterate ((`mod`n) . (^2))
 
 powerMod :: Int -> Int -> Int -> Int
-powerMod n base exp = foldr (prodMod n) 1 $ zipFilter (binary exp) (repeatedSquares n base)
+powerMod n base exp = foldr (prodMod n) 1 $
+    zipFilter (binary exp) (repeatedSquares n base)
 
 zipFilter :: [Bool] -> [a] -> [a]
 zipFilter bs = map snd . filter fst . zip bs
