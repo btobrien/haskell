36b4af1a3fbf818f6650006ea01f830d9f591c23
bret <bobrien@wolve.com>
2021-02-16 18:51:49 -0600

    sage: RSA refinements

--- Sage/Data/Algabraic/Crypt/RSA.hs
+++ Sage/Data/Algabraic/Crypt/RSA.hs
@@ -1,38 +1,46 @@
 
 module Data.Crypt.RSA where
 
-import Utils ((<==>), select, dump)
-import Data.Prime (prime, primes, isPrime, totient)
+import Utils ((<==>))
+import Data.Prime (factor, prime, primes, isPrime, totient)
 import Data.Group (inverse)
 import Data.Group.Modulo (Modulo, modulo, baseOf)
-import Data.Group.Units (Unit, unit, units)
-import Data.Digest.Pure.SHA
-import qualified Data.ByteString.Lazy as B
-import Data.Word
+import Data.Group.Units (Unit, unit)
 
-type Seed = Int
+--import Data.Digest.Pure.SHA
+--import qualified Data.ByteString.Lazy as B
+--import Data.Word
+
+type Prime = Integer
 type Key = Modulo
 
-generate :: Seed -> (Integer,Integer) -> (Key,Key)
---generate seed (p,q) | prime p && prime q = let
-generate seed (p,q) = let
-    n = p * q
-    m = (p-1) * (q-1)
-    e = select seed (units m)
+-- a commonly used encoder because of short bit length and small hamming-weight
+defaultEncoder :: Prime
+defaultEncoder = 2^16 + 1
+
+smallEncoder :: Prime
+smallEncoder = 3
+
+bitLength :: Integer -> Int
+bitLength = (+1) . floor . logBase 2 . fromIntegral
+
+generate :: Prime -> [Prime] -> (Key,Key)
+generate e' pq = let
+    n = product pq
+    m = product . map (subtract 1) $ pq
+    e = unit m e'
     d = inverse e
     in (modulo n e, modulo n d)
 
+-- CRT (ChineseRemainderThm) can be used for more efficent encoding based on factorization...
 encode :: Key -> Integer -> Integer
 encode e x = toInteger $ (modulo (baseOf e) x) ^ e
 
 decode :: Key -> Integer -> Integer
 decode = encode
 
-matching :: (Key,Key) -> Bool
-matching (e,d) = all (decode d . encode e <==> id) [0..(baseOf e)-1]
-
-matchesEvery :: Integer -> (Key,Key) -> Bool
-matchesEvery k (e,d) = all (decode d . encode e <==> id) [0,k..((baseOf e)-1)]
+check :: Integer -> (Key,Key) -> Bool
+check k (e,d) = all (decode d . encode e <==> id) [0,(baseOf e `div` k)..((baseOf e)-1)]
 
 crack :: Key -> Key
 crack e = let n = baseOf e in
