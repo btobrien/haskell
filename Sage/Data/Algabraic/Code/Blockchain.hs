06d78c552f76c12a64826f2bfa4bdc22973089ba
Bret O'Brien <bobrien@umich.edu>
2021-02-20 10:29:33 -0600

    cont bc sketch

--- Sage/Data/Algabraic/Code/Blockchain.hs
+++ Sage/Data/Algabraic/Code/Blockchain.hs
@@ -3,6 +3,8 @@ import Data.Digest.Pure.SHA as SHA
 import Data.ByteString.Lazy (ByteString, pack)
 import Data.Word
 
+import qualified Data.Map.Strict as Map
+
 import qualified Data.Algabraic.Crypt.RSA as RSA
 
 -- how many transactions must occur before a transaction is believed?
@@ -12,30 +14,39 @@ import qualified Data.Algabraic.Crypt.RSA as RSA
 --
 -- how to recover (&publish) chain if lost?
 
-sign :: Show a => RSA.Key -> a -> Signed a
-sign key = RSA.sign (RSA.hash 256) (RSA.defaultEncoder, key)
+hash = RSA.hash 256
 
-type Hash = String
+sign :: Show a => RSA.Key -> a -> Signed a
+sign key = RSA.sign hash (RSA.defaultEncoder, key)
+
+type Hash = Integer
 type UserId = Integer
 
-data Transaction = T { timestamp :: Integer, receiver :: UserId, amount :: Integer }
+data Transaction = T { transactiontime :: Integer, receiver :: UserId, amount :: Integer }
 
-data Block = B { previous :: Hash, transaction :: Signed Transaction, proof :: String }
+data Block = B { blocktime :: Integer, previous :: Hash, transaction :: Signed Transaction, proof :: String }
 
-verify :: (Block,Block) -> Bool
-verify = undefined
+verify :: Block -> Block -> Bool
+verify prev candidate = hash prev
 
-type Chain a = Tree a
+type Chain a = Map.Map Hash (Signed a,[Hash])
+
+-- don't store rejects...no pow
+-- first path, store as little as possible
+
+-- append
+-- balance-request
+-- block-request
+-- lengths
 
 append :: Block -> Chain Block -> Chain Block
 append block chain = let
     prevHash = show . head chain
 
-trim :: Chain Block -> Chain Block
-trim
 balance :: UserId -> [Block] -> Integer
 balance = undefined
 
 balances :: [Block] -> [(UserId,Integer)]
 balances = undefined
 
+-- how to check if block was confirmed
